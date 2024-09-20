e618781d8e92df6adc078d2d53b0069c2ec32373
John <john-romagno@outlook.com>
2024-09-20 13:23:59 -0400

    include frontend in repositiory

---
+++ frontend/src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        John Romagno Personal Training
+      </header>
+    </div>
+  );
+}
+
+export default App;
